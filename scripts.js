e4859507bc3fbf2cdbdea5ef0f56ef05fd339fad
Shevaunie Senior <shevaunie.senior@mymona.uwi.edu>
2025-11-23 15:07:53 -0500

    completed javascript

--- scripts.js
+++ scripts.js
@@ -1,3 +1,26 @@
-document.addEventListener("OnLoaded", function(){
+/*
+	1. get what's in field
+	2. display in div
+*/
+
+document.addEventListener("DOMContentLoaded", function(){
+    let btn = document.querySelector("button"); //button
+    let fld = document.querySelector("input"); //text field
+	let msgDiv = document.getElementsByClassName("message")[0]; //div w/ message
+	
+    btn.addEventListener("click", function(event){
+		event.preventDefault(); //reloaded when field empty so that's removed
+		let fldInfo = fld.value; //get value in the textfield
+		let say = null; //init what message to display
+		
+		if(fldInfo.length>0){
+		say = document.createTextNode("Thank you! Your email address "+ fldInfo +" has been added to our mailing list!");
+		}else{
+		say = document.createTextNode("Please enter a valid email address.");
+		}
+		
+		msgDiv.innerHTML =""; //empty div tag material
+		msgDiv.append(say); //add message to say
+    });
 
 })
